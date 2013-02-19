716a534e1f8a3d9c96f68357c61e9777928af08b
Ian Middleton <ian.r.middleton@gmail.com>
2013-02-18 21:33:14 -0600

    Started working on playback

--- src/VOIP/Sandbox/Listener.java
+++ src/VOIP/Sandbox/Listener.java
@@ -1,8 +1,8 @@
-package VOIP.Sandbox;
-import java.io.File;
+package Sandbox;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
-import javax.sound.sampled.AudioFileFormat;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
@@ -10,8 +10,16 @@ import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.TargetDataLine;
 
+import sun.audio.AudioPlayer;
+
 public class Listener{
-	public static void main(String[] args) throws LineUnavailableException, IOException{		
+	public static void main(String[] args) throws LineUnavailableException, IOException{
+		byte[] buffer = new byte[1000];
+		
+		int numBytesRead = 0;
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		ByteArrayInputStream in = new ByteArrayInputStream(buffer);
+		
 		TargetDataLine line = null;
 		
 		AudioFormat format = new AudioFormat(8000.0f, 16, 1, true, true);
@@ -20,12 +28,18 @@ public class Listener{
 		
 		line = (TargetDataLine)AudioSystem.getLine(dataLineInfo);
 		
+		byte[] data = new byte[line.getBufferSize() / 5];
+		
 		line.open(format);
 		line.start();
 		
-		AudioFileFormat.Type fileType = AudioFileFormat.Type.AIFF;
-		File audioFile = new File("C:/Users/Ian/junk.aif");
+		AudioInputStream as = new AudioInputStream(in, format, data.length);
 		
-		AudioSystem.write(new AudioInputStream(line), fileType, audioFile);
+		while(true){
+			numBytesRead = line.read(data, 0, data.length);
+			out.write(data, 0, numBytesRead);
+			as.read(out.toByteArray(), 0, out.toByteArray().length);
+			AudioPlayer.player.start(as);
+		}
 	}
 }
