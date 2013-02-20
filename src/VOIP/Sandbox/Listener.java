511284a24b39450002f3e3bddb4541b02e2fd2b1
Ian Middleton <ian.r.middleton@gmail.com>
2013-02-19 22:35:11 -0600

    Working Listener!

--- src/VOIP/Sandbox/Listener.java
+++ src/VOIP/Sandbox/Listener.java
@@ -1,47 +1,39 @@
 package Sandbox;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 
-import javax.sound.sampled.AudioFileFormat;
 import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 import javax.sound.sampled.TargetDataLine;
-import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class Listener{
-	public static void main(String[] args) throws LineUnavailableException, IOException, UnsupportedAudioFileException, InterruptedException{
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		AudioFileFormat.Type type = AudioFileFormat.Type.AIFF;
-		
+	public static void main(String[] args) throws LineUnavailableException{		
 		TargetDataLine tLine = null;
 		SourceDataLine sLine = null;
 		
 		AudioFormat format = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 44100,
 				16, 2, 4, 44100, false);		
+		
 		DataLine.Info tLineInfo = new DataLine.Info(TargetDataLine.class, format);
 		DataLine.Info sLineInfo = new DataLine.Info(SourceDataLine.class, format);
 		
 		tLine = (TargetDataLine)AudioSystem.getLine(tLineInfo);
 		sLine = (SourceDataLine)AudioSystem.getLine(sLineInfo);
 		
-		AudioInputStream stream = new AudioInputStream(tLine);
-		
 		tLine.open(format);
 		sLine.open(format);
 		
+		tLine.start();
 		sLine.start();
 		
+		byte[] buffer = new byte[256];
+		
+		int numBytes = 0;
+		
 		while(true){
-			tLine.start();
-			AudioSystem.write(stream, type, out);
-			Thread.sleep(1000);
-			tLine.stop();
-			tLine.flush();
-			sLine.write(out.toByteArray(), 0, out.toByteArray().length);
+			numBytes = tLine.read(buffer, 0, buffer.length);
+			sLine.write(buffer, 0, numBytes);
 		}
 	}
 }
